2d95a169e940772d3cdc95b6cf9849e91c16df0d
Alex Booker <booker@booker.codes>
2017-12-06 16:29:55 +0000

    Add two editors

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/assets'));
+app.set('views', __dirname + '/views');
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render('home');
+app.get('/notes/:noteId', function(req, res) {
+  res.render('note', {noteId: req.params.noteId});
 });
 
 app.listen(PORT, function() {
